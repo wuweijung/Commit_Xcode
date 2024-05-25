963a2b80fdc2ce3c40f51ceef6d928dfcd210ac4
WWJ <justcallme7979@gmail.com>
2024-05-25 13:01:38 +0800

    Initial Commit

---
+++ p01_Hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  p01_Hello
+//
+//  Created by WWJ on 2024/5/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Hello C on macOS with Xcode!\n");
+    return 0;
+}
